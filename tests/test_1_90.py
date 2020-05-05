7a41323c27fdb64fc50ae86357bb2b220c77687f
uber <matias.uberti02@gmail.com>
2020-05-05 15:40:01 -0300

    Agregado el test de 1-90

---
+++ tests/test_1_90.py
@@ -0,0 +1,7 @@
+from src.bingo import carton
+def test_1_90():
+	mi_carton=carton()
+	for fila in range(0,3):
+		for columna in range(0,9):
+			celda = mi_carton[fila][columna]
+			assert celda >=0 and celda <= 90
